d28ab4972fcc4e0d89144f8abe3cb457acb6ecbe
LennyAbels <pinkphantom064@gmail.com>
2025-10-14 18:36:56 -0600

    Titulo Principal Cambiado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Gestión Veterinaria</title>
+    <title>Sistema de Gestión Veterinaria de Chicos Bien Hijos de Cristo Rey</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
